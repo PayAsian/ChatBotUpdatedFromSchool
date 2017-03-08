b0609034a8c760ad1c66718a33b2778d7a986df4
PayAsian <PayAsian@yahoo.com>
2017-03-08 12:45:52 -0700

    i can search through tweets

--- src/chat/model/CtecTwitter.java
+++ src/chat/model/CtecTwitter.java
@@ -2,6 +2,7 @@ package chat.model;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 import chat.controller.ChatbotController;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
@@ -12,13 +13,15 @@ public class CtecTwitter
 {
 	private ChatbotController baseController;
 	private Twitter twitterBot;
-	private List<Status> searchedTweets;
-	private List<String> ignoredWords;
+	private List<String> tweetedWords;
+	private List<Status> allTheTweets;
 	
 	public CtecTwitter(ChatbotController baseController)
 	{
 		this.baseController = baseController;
-		twitterBot = TwitterFactory.getSingleton();
+		this.tweetedWords = new ArrayList<String>();
+		this.allTheTweets = new ArrayList<Status>();
+		this.twitterBot = TwitterFactory.getSingleton();
 	}
 	
 	public void sendTweet(String textToTweet)
@@ -37,23 +40,67 @@ public class CtecTwitter
 		}
 	}
 	
-	private void createIgnoredWordList()
-	{
-		
-	}
-	
-	private void collectTweets(String username)
-	{
-		
-	}
-	
 	public String getMostPopularWord()
 	{
+		removeBoringWords();
+		removeBlankWords();
+		
 		return "";
 	}
 	
 	private String [] createIgnoredWordsArray()
 	{
+		String [] boringWords;
+		int wordCount = 0;
+		
+		Scanner boringWordScanner = new Scanner(this.getClass().getResourceAsStream("commonWords.txt"));
+		while(boringWordScanner.hasNextLine())
+		{
+			wordCount++;
+		}
+		boringWordScanner.close();
+		
+		boringWords = new String [wordCount];
+		
+		boringWordScanner = new Scanner(this.getClass().getResourceAsStream("commonWords.txt"));
+		
+		for(int index = 0; index < boringWords.length; index++)
+		{
+			boringWords[index] = boringWordScanner.next();
+		}
+		boringWordScanner.close();
+			
 		return null;
 	}
+	
+	private void removeBoringWords()
+	{
+		String [] boringWords = createIgnoredWordsArray();
+		
+		for(int index = 0; index < tweetedWords.size(); index++)
+		{
+			for(int boringIndex = 0; boringIndex < boringWords.length; boringIndex++)
+			{
+				if(tweetedWords.get(index).equalsIgnoreCase(boringWords[boringIndex]))
+				{
+					tweetedWords.remove(index);
+					index--;
+					boringIndex = boringWords.length;
+				}
+			}
+		}
+	}
+	private void removeBlankWords()
+	{
+		for(int index = 0; index < tweetedWords.size(); index ++)
+		{
+			if(tweetedWords.get(index).trim().equals(""))
+			{
+				tweetedWords.remove(index);
+				index--;
+				blankIndex = blankWords.length;
+			}
+		}
+	}
+	
 }
